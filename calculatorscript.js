0d80147a022af049c2735e418c836507079aeef9
broop <soundwanders@gmail.com>
2020-09-26 17:06:54 -0400

    created function with if statement checks for user clicking button elements. need to get digits to populate input on button click

--- calculatorscript.js
+++ calculatorscript.js
@@ -14,8 +14,52 @@ function updateDisplay() {
 	// update value of screen element with content of `displayValue`
 	display.value = calcDisplay.displayValue;
 }
-
+//execute the updateDisplay function
 updateDisplay();
+
+// BUTTON CHECKS
+	// adding if statement checks if user has clicked a button element
+	// the 'target' event is whichever button is clicked by the user
+	// if button element is clicked, button type and value will be console logged
+
+const buttons = document.querySelector('.calculatorButtons');
+buttons.addEventListener('click', (e) => {
+  const target = e.target;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('allClear')) {
+    console.log('clearAll', target.value);
+    return;
+  }
+
+  if (target.classList.contains('clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+  
+  	input(target.value);
+	updateDisplay();
+});
+
+
+function input(digit) {
+	const { displayValue } = calculator;
+	// Overwrite `displayValue` if the current value is '0' otherwise append to it
+	calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+  
 
 // MATH FUNCTIONS
 function add () {
@@ -42,6 +86,7 @@ function factorial() {
 	
 }
 
+/*
 module.exports = {
 	add,
 	subtract,
@@ -50,3 +95,4 @@ module.exports = {
     power,
 	factorial
 }
+*/
