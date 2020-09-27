ab59aee0078b38cc5854ad71cb94f4e33946c0f9
broop <soundwanders@gmail.com>
2020-09-26 21:05:42 -0400

    got decimal point button working by checking if decimal point is already there, if not appends a decimal to the calculator display. continue working on math functions

--- assets/calculatorscript.js
+++ assets/calculatorscript.js
@@ -1,6 +1,5 @@
 //calculatorscript.js
-
-// calculator screen
+// Create calculator display 
 const calcDisplay = {
 	displayValue: '0',
 	firstOperand: null,
@@ -8,40 +7,113 @@ const calcDisplay = {
 	operator: null,
 }
 
+// pressing number buttons will input them to calculator display
+function inputDigit(digit) {
+	const { displayValue } = calcDisplay;
+	// Overwrite `displayValue` if the current value is '0' otherwise append
+	calcDisplay.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+
+//update calculator screen display
 function updateDisplay() {
 	const display = document.querySelector('.calculatorDisplay');
-	// update value of screen element with the contents of `displayValue`
+	// update value of screen element with content of `displayValue`
 	display.value = calcDisplay.displayValue;
 }
 
+//execute updateDisplay function
 updateDisplay();
 
+
+// BUTTON CHECKS
+	// adding if statement checks if user has clicked a button element
+	// the 'target' event is whichever button is clicked by the user
+	// if button element is clicked, button type and value will be console logged
+
+const buttons = document.querySelector('.calculatorButtons');
+buttons.addEventListener('click', (e) => {
+  const target = e.target;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('allClear')) {
+    allClear(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+  
+  inputDigit(target.value);
+  updateDisplay();
+  
+});
+
+
 // MATH FUNCTIONS
-
-function add () {
+function add() {
 	
 }
 
-function subtract () {
+function subtract() {
 	
 }
 
-function sum () {
+function sum() {
 	
 }
 
-function multiply () {
+function multiply() {
 	
 }
 
-function power() {
+//pressing decimal button will add decimal point to display, unless one is already there
+function inputDecimal(point) {
+  if (!calcDisplay.displayValue.includes(point)) {
+      calcDisplay.displayValue += point;
+    }
+};
+
+function squared() {
 	
 }
 
-function factorial() {
+function squareRoot() {
+
+}
+
+function equals() {
 	
 }
 
+function allClear() {
+  calcDisplay.innerText != "";
+  calcDisplay.displayValue = '0'
+  console.log("All Cleared");
+}
+
+function clear() {
+	
+};
+
+
+//MODULE EXPORTS
+/*
 module.exports = {
 	add,
 	subtract,
@@ -50,3 +122,4 @@ module.exports = {
     power,
 	factorial
 }
+*/
